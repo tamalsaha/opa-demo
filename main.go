ebcbda11d337c41b1b8a90afa56ec14f16652af8
Tamal Saha <tamal@appscode.com>
2018-07-01 18:09:40 -0700

    added-all

--- main.go
+++ main.go
@@ -1,7 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/open-policy-agent/opa/ast"
+	"github.com/open-policy-agent/opa/rego"
+)
 
 func main() {
-	fmt.Println("Hello, opa")
+	ctx := context.Background()
+
+	// Define a simple policy.
+	module := `
+		package example
+
+		default allow = false
+
+		allow {
+			input.identity = "admin"
+		}
+
+		allow {
+			input.method = "GET"
+		}
+	`
+
+	// Parse the module. The first argument is used as an identifier in error messages.
+	parsed, err := ast.ParseModule("example.rego", module)
+	if err != nil {
+		// Handle error.
+	}
+
+	// Create a new compiler and compile the module. The keys are used as
+	// identifiers in error messages.
+	compiler := ast.NewCompiler()
+	compiler.Compile(map[string]*ast.Module{
+		"example.rego": parsed,
+	})
+
+	if compiler.Failed() {
+		// Handle error. Compilation errors are stored on the compiler.
+		panic(compiler.Errors)
+	}
+
+	// Create a new query that uses the compiled policy from above.
+	rego := rego.New(
+		rego.Query("data.example.allow"),
+		rego.Compiler(compiler),
+		rego.Input(
+			map[string]interface{}{
+				"identity": "bob",
+				"method":   "GET",
+			},
+		),
+	)
+
+	// Run evaluation.
+	rs, err := rego.Eval(ctx)
+
+	if err != nil {
+		// Handle error.
+	}
+
+	// Inspect results.
+	fmt.Println("len:", len(rs))
+	fmt.Println("value:", rs[0].Expressions[0].Value)
 }
